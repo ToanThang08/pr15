65fafb830d2d0471b3d3bb853f8cd1aaddf60a33
Toan Thang <box.dota@gmail.com>
2023-12-26 01:15:14 +0300

    Check delta > 0

--- main.cpp
+++ main.cpp
@@ -16,6 +16,13 @@ int main() {
     // Tính delta
     double delta = b * b - 4 * a * c;
 
-
+    // Kiểm tra giá trị của delta để xác định số nghiệm và tính nghiệm
+    if (delta > 0) {
+        double x1 = (-b + sqrt(delta)) / (2 * a);
+        double x2 = (-b - sqrt(delta)) / (2 * a);
+        cout << "Phương trình có hai nghiệm thực phân biệt:\n";
+        cout << "x1 = " << x1 << "\n";
+        cout << "x2 = " << x2 << "\n";
+    }
 
 }
