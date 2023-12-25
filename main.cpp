d53c6be5fc2313caa2e359571d35327e3a729abc
Toan Thang <box.dota@gmail.com>
2023-12-26 01:06:16 +0300

    program initialization

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    // Nhập các hệ số a, b, c từ người dùng
+    double a, b, c;
+    cout << "Nhập hệ số a: ";
+    cin >> a;
+    cout << "Nhập hệ số b: ";
+    cin >> b;
+    cout << "Nhập hệ số c: ";
+    cin >> c;
+
+}
